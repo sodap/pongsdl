e93d9f5bce1d94b6c40ea874e9aa2724f45c1357
Edu-Desktop <edu1337@gmail.com>
2021-01-20 15:51:25 +0100

    removed unnecesary bat files

--- build/create.bat
+++
@@ -1,3 +0,0 @@
-SET newProject=%1%
-MKDIR W:\%newProject%
-robocopy W:\_ProjectTemplate\ W:\%newProject%\ /e
--- build/play.bat
+++
@@ -1,5 +0,0 @@
-call build
-SET root=w:\pong
-PUSHD "%root%\bin\latest\"
-START latest.exe
-POPD
